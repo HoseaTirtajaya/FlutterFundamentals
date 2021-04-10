7d96fbc9244d1ef5488c6e2ce76e07ecf00760a5
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2021-04-10 21:05:52 +0700

    update last(container pop up not fixed)

--- lib/main.dart
+++ lib/main.dart
@@ -25,48 +25,107 @@ class homepage extends StatelessWidget {
       ),
       body: Center(
         child: RaisedButton(
-          child: Text("Click Me!"),
-          onPressed: () {
-            showDialog(
+            color: Colors.white,
+            splashColor: Colors.white,
+            child: Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/taphere.jpg'),
+                      fit: BoxFit.cover)),
+            ),
+            onPressed: () {
+              showDialog(
                 context: context,
-                builder: (context) {
-                  return Dialog(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0)),
-                    child: Container(
-                      height: 200,
-                      child: Padding(
-                        padding: EdgeInsets.all(12.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            TextField(
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "Hello World"),
-                              textAlign: TextAlign.center,
-                            ),
-                            SizedBox(
-                              width: 320.0,
-                              child: RaisedButton(
-                                color: Colors.blue,
-                                onPressed: () {},
-                                child: Text(
-                                  "Got it!",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                });
-          },
-        ),
+                builder: (context) => CustomDialog(
+                  title: "Success",
+                  description: "Lorem Ipsum dolor sit amet",
+                ),
+              );
+            }),
       ),
+    );
+  }
+}
+
+class CustomDialog extends StatelessWidget {
+  final String title, description, buttonText;
+  final Image image;
+
+  CustomDialog({this.title, this.description, this.buttonText, this.image});
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+
+  dialogContent(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(top: 100, bottom: 16, left: 16, right: 16),
+          margin: EdgeInsets.only(top: 16),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(17),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10.0,
+                    offset: Offset(0.0, 10.0))
+              ]),
+          child: Column(
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    title,
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  Text(
+                    description,
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: FlatButton(
+                      splashColor: Colors.black.withOpacity(0.05),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Got it!"),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 16,
+          right: 16,
+          child: CircleAvatar(
+            backgroundColor: Colors.blue,
+            radius: 50.0,
+            backgroundImage: AssetImage('assets/images/success-mantapu.gif'),
+          ),
+        )
+      ],
     );
   }
 }
