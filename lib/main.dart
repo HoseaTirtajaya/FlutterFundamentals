8fec332f6b62dc4606cd82de329d7a525abeda28
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2021-04-10 19:24:37 +0700

    Creating Alert Button(basic)

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: Text("Container Exercise!")),
-      body: Container(
-        color: Colors.redAccent,
-        margin: EdgeInsets.fromLTRB(8, 16, 16, 8),
-        padding: EdgeInsets.only(top: 16, bottom: 16, left: 24, right: 24),
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: <Color>[
-                Colors.amber,
-                Colors.blue,
-                Colors.purple,
-                Colors.red,
-                Colors.green
-              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-              borderRadius: BorderRadius.circular(16)),
-          margin: EdgeInsets.all(10),
+      title: "PopUp Dialog App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: homepage(),
+    );
+  }
+}
+
+class homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TUGAS ESA UNGGUL"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Click Me!"),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Pop Up Alert!"),
+                    content: Text("Body of Alert"),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text("Got it!"),
+                        onPressed: () {},
+                      )
+                    ],
+                  );
+                });
+          },
         ),
       ),
-    ));
+    );
   }
 }
