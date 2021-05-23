ad55a81f1c37556cef663a281be3ceb63e219cab
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2021-05-23 22:49:14 +0700

    Ujian Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,135 +1,230 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class Product {
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  Product(this.name, this.description, this.price, this.image);
+
+  static List<Product> getProducts() {
+    List<Product> items = <Product>[];
+
+    items.add(Product("Pixel", "Pixel is the most feature-full phone ever", 800,
+        "pixel.jpg"));
+    items.add(Product("Laptop", "Laptop is most productive development tool",
+        2000, "laptop.jpg"));
+    items.add(Product(
+        "Tablet",
+        "Tablet is the most useful device ever for meeting",
+        1500,
+        "tablet.jpg"));
+    items.add(Product(
+        "Pendrive", "Pendrive is useful storage medium", 100, "pendrive.jpg"));
+    items.add(Product("Floppy Drive",
+        "Floppy drive is useful rescue storage medium", 20, "floppy.jpg"));
+    return items;
+  }
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "PopUp Dialog App",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: homepage(),
+      title: 'Shopey',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Product state demo home page'),
     );
   }
 }
 
-class homepage extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  final items = Product.getProducts();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Product Navigation")),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              child: ProductBox(item: items[index]),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProductPage(item: items[index]),
+                  ),
+                );
+              },
+            );
+          },
+        ));
+  }
+}
+
+class ProductPage extends StatelessWidget {
+  ProductPage({Key key, this.item}) : super(key: key);
+  final Product item;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TUGAS ESA UNGGUL"),
-        centerTitle: true,
+        title: Text(this.item.name),
       ),
       body: Center(
-        child: RaisedButton(
-            color: Colors.white,
-            splashColor: Colors.white,
-            child: Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/taphere.jpg'),
-                      fit: BoxFit.cover)),
-            ),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) => CustomDialog(
-                  title: "Success",
-                  description:
-                      "Congratulation! You've tapped the image! Now the pop up has been shown!",
-                ),
-              );
-            }),
+        child: Container(
+          padding: EdgeInsets.all(0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.asset("assets/images/" + this.item.image),
+                Expanded(
+                    child: Container(
+                        padding: EdgeInsets.all(5),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text(this.item.name,
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text(this.item.description),
+                            Text("Price: " + this.item.price.toString()),
+                            RatingBox(),
+                          ],
+                        )))
+              ]),
+        ),
       ),
     );
   }
 }
 
-class CustomDialog extends StatelessWidget {
-  final String title, description, buttonText;
-  final Image image;
-
-  CustomDialog({this.title, this.description, this.buttonText, this.image});
+class RatingBox extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      elevation: 0,
-      backgroundColor: Colors.transparent,
-      child: dialogContent(context),
-    );
+  _RatingBoxState createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+  void _setRatingAsOne() {
+    setState(() {
+      _rating = 1;
+    });
   }
 
-  dialogContent(BuildContext context) {
-    return Stack(
+  void _setRatingAsTwo() {
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAsThree() {
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    double _size = 20;
+    print(_rating);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
       children: <Widget>[
         Container(
-          width: 280,
-          height: 300,
-          padding: EdgeInsets.only(top: 100, bottom: 16, left: 16, right: 16),
-          margin: EdgeInsets.only(top: 16),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(17),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 10.0,
-                    offset: Offset(0.0, 10.0))
-              ]),
-          child: Column(
-            children: <Widget>[
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    title,
-                    style:
-                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700),
-                  ),
-                  SizedBox(
-                    height: 24.0,
-                  ),
-                  Text(
-                    description,
-                    style: TextStyle(fontSize: 16.0),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: 24.0,
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: FlatButton(
-                      splashColor: Colors.black.withOpacity(0.05),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text("Got it!"),
-                    ),
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1
+                ? Icon(
+                    Icons.star,
+                    size: _size,
                   )
-                ],
-              )
-            ],
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
           ),
         ),
-        Positioned(
-          top: 0,
-          left: 16,
-          right: 16,
-          child: CircleAvatar(
-            backgroundColor: Colors.blue,
-            radius: 50.0,
-            backgroundImage: AssetImage('assets/images/success-mantapu.gif'),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 2
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsTwo,
+            iconSize: _size,
           ),
-        )
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 3
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsThree,
+            iconSize: _size,
+          ),
+        ),
       ],
     );
+  }
+}
+
+class ProductBox extends StatelessWidget {
+  ProductBox({Key key, this.item}) : super(key: key);
+  final Product item;
+
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(2),
+        height: 140,
+        child: Card(
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Image.asset("assets/images/" + this.item.image),
+                Expanded(
+                    child: Container(
+                        padding: EdgeInsets.all(5),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text(this.item.name,
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text(this.item.description),
+                            Text("Price: " + this.item.price.toString()),
+                            RatingBox(),
+                          ],
+                        )))
+              ]),
+        ));
   }
 }
